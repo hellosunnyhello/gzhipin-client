e38fe7aa16cdefe8eef0bac5f833e00414b53f66
hellosunnyhello <673761106@qq.com>
2018-08-11 11:45:16 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React,{Component} from 'react';
+import {Button} from 'antd-mobile'
+
+class App extends Component{
+
+    render(){
+        return <Button type='primary'>Hello React</Button>
+    }
+}
+
+export  default App;
